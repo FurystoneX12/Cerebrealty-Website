88c9f33ef87fc75d09ff65780d1ba51525b28418
Joey Santos <furystone99@yahoo.com>
2021-04-06 11:25:11 +0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :listings
+  #get 'home/index'
+  get "home/about"
+  get "home/contact_us"
+  get "home/terms_and_conditions"
+  root "home#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ test/controllers/listings_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class ListingsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @listing = listings(:one)
+  end
+
+  test "should get index" do
+    get listings_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_listing_url
+    assert_response :success
+  end
+
+  test "should create listing" do
+    assert_difference('Listing.count') do
+      post listings_url, params: { listing: { bathrooms: @listing.bathrooms, bedrooms: @listing.bedrooms, car_park: @listing.car_park, floor_area: @listing.floor_area, lot_area: @listing.lot_area, object: @listing.object, type: @listing.type } }
+    end
+
+    assert_redirected_to listing_url(Listing.last)
+  end
+
+  test "should show listing" do
+    get listing_url(@listing)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_listing_url(@listing)
+    assert_response :success
+  end
+
+  test "should update listing" do
+    patch listing_url(@listing), params: { listing: { bathrooms: @listing.bathrooms, bedrooms: @listing.bedrooms, car_park: @listing.car_park, floor_area: @listing.floor_area, lot_area: @listing.lot_area, object: @listing.object, type: @listing.type } }
+    assert_redirected_to listing_url(@listing)
+  end
+
+  test "should destroy listing" do
+    assert_difference('Listing.count', -1) do
+      delete listing_url(@listing)
+    end
+
+    assert_redirected_to listings_url
+  end
+end
