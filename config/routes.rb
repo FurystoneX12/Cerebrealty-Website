d3f8dd77212f3ea5c72be96786703887c8eaab67
Joey Santos <furystone99@yahoo.com>
2021-04-17 22:56:13 +0800

    added articles page, teaked other pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "home/about"
   get "home/contact_us"
   get "home/terms_and_conditions"
+  get "home/articles"
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
