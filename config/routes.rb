d5150b82e222f31fc7518a448948249c10fc1b70
Joey Santos <furystone99@yahoo.com>
2021-04-18 01:12:56 +0800

    tweaked index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get "home/contact_us"
   get "home/terms_and_conditions"
   get "home/articles"
+  get "home/real_estate"
+  get "home/law"
+  get "home/engineering"
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
