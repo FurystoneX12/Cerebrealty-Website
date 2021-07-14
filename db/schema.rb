b9bad73761477b9e840a5bae996f76c38eff54ce
Joey Santos <furystone99@yahoo.com>
2021-07-14 12:59:34 +0800

    added comma to large numbers

--- db/schema.rb
+++ db/schema.rb
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 2021_06_23_120621) do
   create_table "real_estates", force: :cascade do |t|
     t.string "offer_type"
     t.string "object"
-    t.float "land_area"
-    t.float "floor_area"
+    t.decimal "land_area"
+    t.decimal "floor_area"
     t.string "town_and_province"
     t.integer "bedroom"
     t.integer "bathroom"
